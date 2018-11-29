103e08650802093a434b90c3a71725f1110afa81
github username <bteng1226@gmail.com>
2018-11-28 19:16:21 -0500

    fixed solution for convert_to_camel_case

--- ruby/convert_to_camel_case.rb
+++ ruby/convert_to_camel_case.rb
@@ -1,28 +1,12 @@
+# Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized.
+
 def to_camel_case(str)
-if str == ""
-return str
-end
-
-  index = 0
-  while index < str.length
-  if str[index] == "-"
-    str = str.split("-")
-    end
-  if str[index] == "_"
-    str=str.split("_")
-    end
-    index +=1
+  temp = str.split(/[-_]/)
+  ret =[]
+  temp.each_with_index do |word,index|
+    index != 0 ? ret.push(word.capitalize) : ret.push(word)
   end
-newString =str[0]
-index2=1
-while index2 < str.length
-str[index2] = str[index2].capitalize
-newString += str[index2]
-index2+=1
+  ret ? ret.join("") : str
 end
 
-newString
-
-end
-
-p to_camel_case("this is a test string")
+p to_camel_case("this-is_a-test_string")
