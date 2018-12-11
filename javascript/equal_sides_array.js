706564a5dcc3ff2e7a1b2a6c54fc5e73acdb9b14
github username <bteng1226@gmail.com>
2018-12-10 19:43:35 -0500

    added tests and it correctly returns the correct answer for all test cases

--- javascript/equal_sides_array.js
+++ javascript/equal_sides_array.js
@@ -27,3 +27,8 @@ function findEvenIndex(arr)
   }
   return -1
 }
+
+console.log(findEvenIndex([1,2,3,4,3,2,1]) === 3);
+console.log(findEvenIndex([1,100,50,-51,1,1]) === 1);
+console.log(findEvenIndex([1,2,3,4,5,6]) === -1);
+console.log(findEvenIndex([20,10,30,10,10,15,35]) === 3)
