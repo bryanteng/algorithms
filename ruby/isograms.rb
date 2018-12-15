44b1e8c9ba2c4af2fb980581700f222231095854
github username <bteng1226@gmail.com>
2018-12-14 22:08:44 -0500

    added isograms

---
+++ ruby/isograms.rb
@@ -0,0 +1,6 @@
+# 7 kyu kata
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+def is_isogram(string)
+  string.downcase.chars.uniq.length == string.length
+end
