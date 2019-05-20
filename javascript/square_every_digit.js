d8b2b552d5ee6b199171fdd9972e9a1ae115f678
Bryan Teng <bteng1226@gmail.com>
2019-05-20 09:25:05 -0500

    added square every digit

---
+++ javascript/square_every_digit.js
@@ -0,0 +1,6 @@
+// 6 kyu kata
+// https://www.codewars.com/kata/one-line-task-square-every-digit/train/javascript
+
+// Inspired by Square Every Digit (and by the inimitable myjinxin2015's many clever one-line kata), your goal here is precisely the same (to square every digit in the given number), in 36 or fewer characters (JavaScript), or 47 or fewer in Ruby
+
+sd=x=>+[...``+x].map(x=>x*x).join``
