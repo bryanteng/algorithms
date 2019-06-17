a6579f755f8a83459a18c1c2a0aa192e89959c03
Bryan Teng <bteng1226@gmail.com>
2019-06-17 09:31:26 -0500

    added Scale

---
+++ CSharp/Scale.cs
@@ -0,0 +1,40 @@
+// 7 kyu kata
+// https://www.codewars.com/kata/56ed20a2c4e5d69155000301
+
+// You are given a string of n lines, each substring being n characters long. For example:
+
+// s = "abcd\nefgh\nijkl\nmnop"
+
+// We will study the "horizontal" and the "vertical" scaling of this square of strings.
+
+// A k-horizontal scaling of a string consists of replicating k times each character of the string (except '\n').
+
+// Example: 2-horizontal scaling of s: => "aabbccdd\neeffgghh\niijjkkll\nmmnnoopp"
+// A v-vertical scaling of a string consists of replicating v times each part of the squared string.
+
+// Example: 2-vertical scaling of s: => "abcd\nabcd\nefgh\nefgh\nijkl\nijkl\nmnop\nmnop"
+// Function scale(strng, k, v) will perform a k-horizontal scaling and a v-vertical scaling.
+
+// Example: a = "abcd\nefgh\nijkl\nmnop"
+// scale(a, 2, 3) --> "aabbccdd\naabbccdd\naabbccdd\neeffgghh\neeffgghh\neeffgghh\niijjkkll\niijjkkll\niijjkkll\nmmnnoopp\nmmnnoopp\nmmnnoopp"
+
+using System;
+using System.Collections.Generic;
+
+public class Scaling
+{
+    public static string Scale(string strng, int k, int n) 
+    {
+        if(strng == "") return "";
+        var vals = new List<string>(strng.Split("\n"));
+        var ret = new List<string>();
+        foreach(string s in vals){
+          string temp="";
+          foreach(char i in s){
+            temp+=i.ToString().PadRight(k,i);
+          }
+          for (int i = 0; i < n; i++) ret.Add(temp);
+        }
+        return String.Join("\n",ret);
+    }
+}
