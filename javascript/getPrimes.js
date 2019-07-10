80c1f49b0a2ec419c1cc741194bc9e958465eabd
Bryan Teng <bteng1226@gmail.com>
2019-07-10 08:53:36 -0500

    added get primes

---
+++ javascript/getPrimes.js
@@ -0,0 +1,37 @@
+// 5 kyu kata
+// https://www.codewars.com/kata/52dd72494367608ac1000416/solutions/javascript/me/best_practice
+
+// You will need to create logic for the following two functions: isPrime(number) and getPrimes(start, finish)
+
+// isPrime(number)
+// Should return boolean true if prime, otherwise false
+
+// getPrimes(start, finish)
+// Should return a unique, sorted array of all primes in the range [start, finish] (i.e. both numbers inclusive). Note that this range can go both ways - e.g. getPrimes(10, 1) should return all primes from 1 to 10 both inclusive.
+
+// Sample Input:
+// isPrime(number):
+
+// isPrime(0); // === false
+// isPrime(1); // === false
+// isPrime(2); // === true
+// isPrime(3); // === true
+// isPrime(4); // === false
+// isPrime(5); // === true 
+// getPrimes(start, finish):
+
+// getPrimes(0, 0); // === []
+// getPrimes(0, 30); // === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+// getPrimes(30, 0); // === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+function isPrime(num){
+    if (num<2) return false
+    for(let i = 2; i <= Math.sqrt(num); i++){
+      if(num%i ==0) return false
+    }    
+    return true
+  }
+  
+  function getPrimes(start, finish) {
+    return [...Array(Math.max(start,finish)+1).keys()].filter(x=> x >= Math.min(start,finish)).filter(x=>isPrime(x))
+  }
