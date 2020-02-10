8bcec08d3eb9c016bbe5946003d09d76be56fa7b
Bryan Teng <bteng1226@gmail.com>
2020-02-09 19:27:17 -0600

    added letter combinations

---
+++ python/letter_combinations.py
@@ -0,0 +1,31 @@
+difficulty: medium
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+Example:
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
+def letterCombinations(self, digits):
+    if digits == "":
+        return []
+    ret = []
+    phone = {'2': ['a', 'b', 'c'],
+          '3': ['d', 'e', 'f'],
+          '4': ['g', 'h', 'i'],
+          '5': ['j', 'k', 'l'],
+          '6': ['m', 'n', 'o'],
+          '7': ['p', 'q', 'r', 's'],
+          '8': ['t', 'u', 'v'],
+          '9': ['w', 'x', 'y', 'z']}
+    def recursive(letters,next_digit):
+        if len(next_digit) == 0:
+            ret.append(letters)
+        else:
+            for i in phone[next_digit[0]]:
+                recursive(letters+i, next_digit[1:])
+
+    recursive("",digits)
+    return ret
