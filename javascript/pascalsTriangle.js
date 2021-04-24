4ad0bfb72e2f78c7640d8ebdc57af6159a64f847
Bryan Teng <bteng1226@gmail.com>
2021-04-23 22:29:47 -0400

    added pascals triangle

---
+++ javascript/pascalsTriangle.js
@@ -0,0 +1,43 @@
+// difficulty:easy
+// https://leetcode.com/problems/pascals-triangle/
+//
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+//
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+// Example 1:
+//
+// Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+//
+// Example 2:
+// Input: numRows = 1
+// Output: [[1]]
+
+
+var generate = function(numRows) {
+    if( numRows == 0 ) return []
+    let ret = [[1]]
+    if( numRows == 1 ) return ret
+    ret.push([1,1])
+    if ( numRows == 2 ) return ret
+    let i = 2
+    let j
+    let k = 0
+    let prev
+    let temp
+    let rev
+    while( i < numRows ){
+      k = 0
+      prev = ret[i-1]
+      temp = [1]
+      j = Math.floor( i / 2)
+      while(k < j){
+        temp.push(prev[k]+ prev[k+1])
+        k++
+      }
+      rev = temp.slice(0, (i+1)/2 ).reverse()
+      ret.push(temp.concat(rev))
+      i++
+    }
+    return ret
+};
